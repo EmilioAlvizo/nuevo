b97da70f91967ae3afdb11b07f002a9f34847f44
Emilio Alvizo Velazquez <emilio_alvizo@yahoo.com.mx>
2025-10-07 10:58:59 -0600

    agregamos los archivos de sandy

--- frontend/app/src/app/app.ts
+++ frontend/app/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Topbar } from './components/topbar/topbar';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Topbar, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app');
+  protected readonly title = signal('sii');
 }
