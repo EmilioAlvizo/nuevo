c415b15b10ac45c51224c87cf74c32439fd2e335
Sandra Karina Alvarez Gonzalez <salvarez@juventudesgto.gob.mx>
2025-10-09 11:55:24 -0600

    home + contactanos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Estadisticas } from './pages/estadisticas/estadisticas';
 import { RevistaVoces } from './pages/revista-voces/revista-voces';
 import { CentroDocumental } from './pages/centro-documental/centro-documental';
 import { InformacionInteres } from './pages/informacion-interes/informacion-interes';
+import { Contactanos } from './components/contactanos/contactanos';
 
 export const routes: Routes = [
     {path: '', component: Home},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path: 'estadisticas', component: Estadisticas},
     {path: 'revista', component: RevistaVoces},
     {path: 'centro-documental', component: CentroDocumental},
-    {path: 'informacion-interes', component: InformacionInteres}
+    {path: 'informacion-interes', component: InformacionInteres},
+    {path: 'contactanos', component: Contactanos},
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,11 +4,12 @@ import { Topbar } from './components/topbar/topbar';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
 import { Prueba } from './components/prueba/prueba';
+import { BotonContactanos } from './components/boton-contactanos/boton-contactanos';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Topbar, Navbar, Prueba],
+  imports: [RouterOutlet, Footer, Topbar, Navbar, Prueba, BotonContactanos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
