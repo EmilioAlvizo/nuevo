f4a364b90c8d46d16ae5c44765f640e5ad75da4e
Emilio Alvizo Velazquez <emilio_alvizo@yahoo.com.mx>
2025-10-19 22:56:49 -0600

    cambios de footer y barra de navegacion

--- frontend/src/app/public/public-layout/public-layout.ts
+++ frontend/src/app/public/public-layout/public-layout.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Topbar } from '../../shared/topbar/topbar';
 import { Navbar } from '../shared/navbar/navbar';
 import { Footer } from '../../shared/footer/footer';
+import { Navbar2 } from '../shared/navbar2/navbar2';
 import { BotonContactanos } from '../shared/boton-contactanos/boton-contactanos';
 
 @Component({
   selector: 'public-layout',
   standalone: true,
-  imports: [RouterOutlet, Topbar, Navbar, Footer, BotonContactanos],
+  imports: [RouterOutlet, Topbar, Navbar, Footer, BotonContactanos, /*Navbar2*/],
   templateUrl: './public-layout.html',
   styleUrls: ['./public-layout.css'],
   encapsulation: ViewEncapsulation.None
---
+++ frontend/src/app/public/shared/navbar2/navbar2.ts
@@ -0,0 +1,54 @@
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar2',
+  imports: [RouterModule],
+  templateUrl: './navbar2.html',
+  styleUrl: './navbar2.css'
+})
+export class Navbar2 implements OnInit, OnDestroy{
+  // Estados reactivos (Angular 20)
+  showTopBar = true;
+  mobileMenuOpen = false;
+  dropdownOpen = false;
+
+  private clickOutsideHandler!: () => void;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    // Detectar clic fuera del dropdown
+    this.clickOutsideHandler = this.renderer.listen('document', 'click', (event) => {
+      const insideDropdown = event.target.closest('.nav-dropdown');
+      if (!insideDropdown) {
+        this.dropdownOpen = false;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.clickOutsideHandler) this.clickOutsideHandler();
+  }
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  toggleDropdown(event: Event): void {
+    event.stopPropagation();
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.mobileMenuOpen = false;
+  }
+
+  // Ocultar barra superior con scroll
+  @HostListener('window:scroll')
+  onScroll(): void {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    console.log('Scroll position:', scrollTop);
+    this.showTopBar = scrollTop < 50;
+  }
+}
